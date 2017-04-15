58bb09006e59d83be7d5bb28b90327509f343e71
cellva <selvanathan@ideas2it.com>
2017-04-16 02:03:03 +0530

    Feature: Server, configuration and property file set up.
      - Asset management
      - Express configuration.
      - Database configuration.
      - Initial route configuration at server side.
      - Property file set up for assets, database, environment and express framework.
      - Node server set up.

---
+++ modules/turtle/server/routes/turtle-routes.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var assetmanager = require('assetmanager');
+
+var assets = assetmanager.process({
+    assets: require(path.join(__dirname, '../../../../properties/assets.json')),
+    debug: (process.env.NODE_ENV !== 'production'),
+    webroot: '/'
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Turtle coding challenge ....',
+        assets: assets
+    });
+});
+
+module.exports = router;
