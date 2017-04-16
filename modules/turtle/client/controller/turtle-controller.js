9578b661c55b726e300f387fb5eebfecd16b0692
cellva <selvanathan@ideas2it.com>
2017-04-17 04:36:02 +0530

    Feature: Turtle img transition.

--- modules/turtle/client/controller/turtle-controller.js
+++ modules/turtle/client/controller/turtle-controller.js
@@ -2,6 +2,7 @@
 
 /**
  * Controller - turtleChallengeController is created
+ * @author  selvanathan
  */
 angular.module("turtleChallenge")
     .controller('turtleChallengeController', turtleChallengeController);
@@ -10,57 +11,97 @@ angular.module("turtleChallenge")
  * [$injector description - Dependency injection for the 'turtleChallengeController' controller is done. ]
  * @type {Array}
  */
-turtleChallengeController.$injector = ['TurtleChallengeService'];
+turtleChallengeController.$injector = ['TurtleChallengeService', '$timeout'];
 
 /**
  * [turtleChallengeController description - Actual 'turtleChallengeController' controller.]
  * @method turtleChallengeController
  * @return {[type]}                  [description]
  */
-function turtleChallengeController(TurtleChallengeService) {
+function turtleChallengeController(TurtleChallengeService, $timeout) {
     var vm = this;
     vm.grid = TurtleChallengeService.grid;
-    vm.obstrucle = TurtleChallengeService.obstrucle;
-    vm.gridSize = 8
+    vm.obstrucles = TurtleChallengeService.obstrucles;
+    vm.gridSize = 10;
     vm.cells = TurtleChallengeService.cells;
     vm.currentCell = {
         x: 1,
         y: 1
     };
-    vm.directionCode = {
-        'R': 1,
-        'L': -1
-    };
     vm.availableDirections = ['W', 'N', 'E', 'S'];
-    vm.currentDirection = '2';
+    vm.currentDirection = 1;
 
     TurtleChallengeService.generateGrid(vm.gridSize, vm.cells, vm.grid);
-    TurtleChallengeService.generateObstrucles(vm.cells, vm.obstrucle, vm.gridSize, vm.grid);
+    TurtleChallengeService.generateObstrucles(vm.cells, vm.obstrucles, vm.gridSize, vm.grid);
 
     vm.findObstrucle = function (cell) {
-        var index = _.findIndex(vm.obstrucle, {
+        var index = _.findIndex(vm.obstrucles, {
             x: cell.x,
             y: cell.y
         });
         return index > -1 ? 'obstrucle' : 'normal';
     }
 
+    /**
+     * [move description]
+     * @method move
+     * @return {[type]} [description]
+     */
     vm.move = function () {
-
-        _.each(inputDirections, function (nextMove) {
-            TurtleChallengeService.calculateNextPosition(vm.currentCell, vm.currentDirection, nextMove);
+        var position = $("#turtle").position(); //Holds the current position of the turtle.
+        var params = {
+                currentPosition: position,
+                currentCell: vm.currentCell,
+                currentDirection: vm.currentDirection,
+                gridSize: vm.gridSize,
+                cellwidth: vm.cellSize
+            },
+            rotate = 0;
+        vm.inputDirections = vm.inputDirections.toUpperCase();
+        _.each(vm.inputDirections, function (nextMove) {
+            params.nextMove = nextMove;
+            position = TurtleChallengeService.calculateNextPosition(params);
+            vm.currentDirection = position.currentDirection;
+            vm.currentCell.x = position.x;
+            vm.currentCell.y = position.y;
+            rotate = (nextMove === "R") ? 90 : -90;
+            $("#turtle").css({
+                "-webkit-transform": "rotate(" + rotate + "deg)",
+                "transition-delay": "1s",
+                "transition-duration": "1s"
+            });
+            $("#turtle").css({
+                "-webkit-transform": "translate(" + position.left + "px, " + position.top + "px)",
+                "transition-delay": "1s",
+                "transition-duration": "1s"
+            });
         });
+        vm.gridPosition = position;
+    }
 
-        TurtleChallengeService.checkForObstrucle()
-        var temp = {
-            x: vm.currentCell.x,
-            y: vm.currentCell.y + 1
-        };
-        var position = $("#turtle").position();
-        position.left = position.left + vm.cellSize;
-        position.top = position.top;
+    /**
+     * [reset description - Resets the turtle to its initial position]
+     * @method reset
+     */
+    vm.reset = function () {
         $("#turtle").css({
-            "-webkit-transform": "translate(" + position.left + "px, " + position.top + "px)"
-        }).delay(2000);
+            "-webkit-transform": "translate(21px,391px)",
+            "transition-delay": "1s",
+            "transition-duration": "1s"
+        });
+        //vm.currentDirection = 1;
+    }
+
+    /**
+     * [restrictInput description - Allow only F-forward, L- left, R- right]
+     * @method restrictInput
+     * @param  {[type]}      $event [description]
+     * @return {[type]}             [description]
+     */
+    vm.restrictInput = function ($event) {
+        var keycode = [76, 70, 82, 13, 20, 16, 8, 17];
+        if(_.indexOf(keycode, $event.keyCode) == -1) {
+            $event.preventDefault();
+        }
     }
 }
--- modules/turtle/client/service/turtle-service.js
+++ modules/turtle/client/service/turtle-service.js
@@ -1,19 +1,38 @@
 "use strict";
 
-
+/**
+ * Creating the 'turtleChallengeService' service
+ * @author  Selvanathan
+ */
 angular.module("turtleChallenge")
     .service('TurtleChallengeService', turtleChallengeService);
 
+/**
+ * [$injector description - dependency injection for th 'turtleChallengeService' service]
+ * @type {Array}
+ */
 turtleChallengeService.$injector = [];
 
-
+/**
+ * [turtleChallengeService description - Angular service for turtle service]
+ * @method turtleChallengeService
+ * @return {[type]}               [description]
+ */
 function turtleChallengeService() {
     var self = this;
     var availableDirections = ['W', 'N', 'E', 'S'];
-    self.obstrucle = [];
+    self.obstrucles = [];
     self.grid = [];
     self.cells = [];
 
+    /**
+     * [generateGrid description - Created a n * n grid.]
+     * @method generateGrid
+     * @param  {[type]}     size  [description]
+     * @param  {[type]}     cells [description]
+     * @param  {[type]}     grid  [description]
+     * @return {[type]}           [description]
+     */
     self.generateGrid = function (size, cells, grid) {
         var min = 2,
             max = size;
@@ -35,25 +54,82 @@ function turtleChallengeService() {
     }
 
 
-    self.generateObstrucles = function (cells, obstrucle, size, grid) {
+    /**
+     * [generateObstrucles description - Generates the obstrucles.]
+     * @method generateObstrucles
+     * @param  {[type]}           cells      [description]
+     * @param  {[type]}           obstrucles [description]
+     * @param  {[type]}           size       [description]
+     * @param  {[type]}           grid       [description]
+     * @return {[type]}                      [description]
+     */
+    self.generateObstrucles = function (cells, obstrucles, size, grid) {
         var min = 1,
             max = size * size;
         for(var index = 1; index <= size; index++) {
             min = index === size ? 3 : 1;
-            obstrucle.push(grid[index - 1][_.random(min, size) - 1])
+            obstrucles.push(grid[index - 1][_.random(min, size) - 1])
         }
     }
 
-    self.checkForObstrucle = function (direction, currentPosition) {
-
+    /**
+     * [checkForObstrucle description - used to check for the obstrucles]
+     * @method checkForObstrucle
+     * @param  {[type]}          position   [description]
+     * @param  {[type]}          obstrucles [description]
+     * @return {[type]}                     [description]
+     */
+    self.checkForObstrucle = function (position, obstrucles) {
+        return _.findIndex(obstrucles, {
+            x: position.x,
+            y: position.y
+        }) > -1 ? true : false;
     }
 
-    self.calculateNextPosition = function (currentCell, currentDirection, nextMove) {
+    /**
+     * [calculateNextPosition description - Calculates the next position form the current given position]
+     * @method calculateNextPosition
+     * @param  {[type]}              params [description]
+     * @return {[type]}                     [description]
+     */
+    self.calculateNextPosition = function (params) {
+
         var directionCode = {
-            'R': 1,
-            'L': -1
-        };
-        nextMove !== 'F' ? availableDirections[(currentDirection % 4) + (directionCode[nextMove])] : currentDirection;
-        console.log(nextMove);
+                'R': 1,
+                'L': -1
+            },
+            newPosition = params.currentPosition,
+            inputs = _.clone(params);
+
+        newPosition.x = inputs.currentCell.x;
+        newPosition.y = inputs.currentCell.y;
+        params.currentDirection = params.nextMove !== 'F' ? (params.currentDirection + directionCode[params.nextMove]) % 4 : params.currentDirection;
+        if(params.nextMove === 'F') {
+            if(availableDirections[params.currentDirection] === 'E' && params.currentCell.y + 1 <= params.gridSize) {
+                newPosition.left = params.currentPosition.left + params.cellwidth;
+                newPosition.x = params.currentCell.x;
+                newPosition.y = params.currentCell.y + 1;
+            } else if(availableDirections[params.currentDirection] === 'N' && params.currentCell.x + 1 <= params.gridSize) {
+                newPosition.top = params.currentPosition.top - params.cellwidth;
+                newPosition.x = params.currentCell.x + 1;
+                newPosition.y = params.currentCell.y;
+            } else if(availableDirections[params.currentDirection] === 'W' && params.currentCell.y - 1 >= 1) {
+                newPosition.left = params.currentPosition.left - params.cellwidth;
+                newPosition.x = params.currentCell.x;
+                newPosition.y = params.currentCell.y - 1;
+            } else if(availableDirections[params.currentDirection] === 'S' && params.currentCell.x - 1 >= 1) {
+                newPosition.top = params.currentPosition.top + params.cellwidth;
+                newPosition.x = params.currentCell.x - 1;
+                newPosition.y = params.currentCell.y;
+            }
+        }
+
+        if(self.checkForObstrucle(newPosition, self.obstrucles)) {
+            newPosition = inputs.currentPosition;
+            // newPosition.left = $("#turtle").position().left;
+            // newPosition.top = $("#turtle").position().top;
+        }
+        newPosition.currentDirection = params.currentDirection;
+        return newPosition;
     }
 }
